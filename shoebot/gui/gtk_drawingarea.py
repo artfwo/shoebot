c7ade4a3bf92ba7c35bdb1291fdf50cb542cb7ac
Stuart Axon <stuaxo2@yahoo.com>
2014-01-03 09:51:49 +0000

    CairoCffi with Gtk2

--- shoebot/gui/gtk_drawingarea.py
+++ shoebot/gui/gtk_drawingarea.py
@@ -1,11 +1,35 @@
 #from __future__ import division
 import sys, os
 import gtk
-import cairocffi as cairo
+import cairo
+import cairocffi
 from socket_server import SocketServerMixin
 
 from shoebot.core import DrawQueueSink
-#from shoebot.util import RecordingSurface
+
+
+def _UNSAFE_pycairo_context_to_cairocffi(pycairo_context, incref=True):
+    # Sanity check. Continuing with another type would probably segfault.
+    if not isinstance(pycairo_context, cairo.Context):
+        raise TypeError('Expected a cairo.Context, got %r' % pycairo_context)
+
+    # On CPython, id() gives the memory address of a Python object.
+    # pycairo implements Context as a C struct:
+    #     typedef struct {
+    #         PyObject_HEAD
+    #         cairo_t *ctx;
+    #         PyObject *base;
+    #     } PycairoContext;
+    # Still on CPython, object.__basicsize__ is the size of PyObject_HEAD,
+    # ie. the offset to the ctx field.
+    # ffi.cast() converts the integer address to a cairo_t** pointer.
+    # [0] dereferences that pointer, ie. read the ctx field.
+    # The result is a cairo_t* pointer that cairocffi can use.
+    return cairocffi.Context._from_pointer(
+        cairocffi.ffi.cast('cairo_t **',
+                           id(pycairo_context) + object.__basicsize__)[0],
+        incref=True)
+
 
 ICON_FILE = os.path.join(sys.prefix, 'share', 'pixmaps', 'shoebot-ide.png')
 
@@ -24,19 +48,19 @@ class ShoebotWidget(gtk.DrawingArea, DrawQueueSink, SocketServerMixin):
 
         # Default picture is the shoebot icon
         if os.path.isfile(ICON_FILE):
-            self.backing_store = cairo.ImageSurface.create_from_png(ICON_FILE)
+            self.backing_store = cairocffi.ImageSurface.create_from_png(ICON_FILE)
         else:
-            self.backing_store = cairo.ImageSurface(cairo.FORMAT_ARGB32, 400, 400)
+            self.backing_store = cairocffi.ImageSurface(cairocffi.FORMAT_ARGB32, 400, 400)
         self.size = None
         self.first_run = True
-	self.last_rendering = None
+        self.last_rendering = None
 
     def do_expose_event(self, event):
         '''
         Draw just the exposed part of the backing store
         '''
         # Create the cairo context
-        cr = self.window.cairo_create()
+        cr = _UNSAFE_pycairo_context_to_cairocffi(self.window.cairo_create())
         if self.scale_fit:
             source_width = self.backing_store.get_width()
             source_height = self.backing_store.get_height()
@@ -66,9 +90,9 @@ class ShoebotWidget(gtk.DrawingArea, DrawQueueSink, SocketServerMixin):
         cr.rectangle(event.area.x, event.area.y,
                 event.area.width, event.area.height)
         if self.first_run:
-            cr.set_operator(cairo.OPERATOR_OVER)
+            cr.set_operator(cairocffi.OPERATOR_OVER)
         else:
-            cr.set_operator(cairo.OPERATOR_SOURCE)
+            cr.set_operator(cairocffi.OPERATOR_SOURCE)
         cr.fill()
 
     def create_rcontext(self, size, frame):
@@ -84,8 +108,8 @@ class ShoebotWidget(gtk.DrawingArea, DrawQueueSink, SocketServerMixin):
             while gtk.events_pending():
                 gtk.main_iteration_do(block=False)
         extents = (0, 0, size[0], size[1])
-        meta_surface = cairo.RecordingSurface(cairo.CONTENT_COLOR_ALPHA, extents)
-        return cairo.Context(meta_surface)
+        meta_surface = cairocffi.RecordingSurface(cairocffi.CONTENT_COLOR_ALPHA, extents)
+        return cairocffi.Context(meta_surface)
 
     def rendering_finished(self, size, frame, cairo_ctx):
         '''
@@ -95,11 +119,11 @@ class ShoebotWidget(gtk.DrawingArea, DrawQueueSink, SocketServerMixin):
         if (self.backing_store.get_width(), self.backing_store.get_height()) == size:
             backing_store = self.backing_store
         else:
-            backing_store = cairo.ImageSurface(cairo.FORMAT_ARGB32, *size)
+            backing_store = cairocffi.ImageSurface(cairocffi.FORMAT_ARGB32, *size)
         
-        cr = cairo.Context(backing_store)
+        cr = cairocffi.Context(backing_store)
         cr.set_source_surface(cairo_ctx.get_target())
-        cr.set_operator(cairo.OPERATOR_SOURCE)
+        cr.set_operator(cairocffi.OPERATOR_SOURCE)
         cr.paint()
         
         self.backing_store = backing_store
