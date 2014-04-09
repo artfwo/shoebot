f1e13785adc6f9e45ffcdb11e68be07f087c2d48
Stuart Axon <stuaxo2@yahoo.com>
2014-04-09 23:44:47 +0100

    Fix tests - eyeballed the colours and it fixes the bug.

--- shoebot/grammar/bot.py
+++ shoebot/grammar/bot.py
@@ -202,7 +202,7 @@ class Bot(Grammar):
     def Ellipse(self, *args, **kwargs):
         return self._makeInstance(Ellipse, args, kwargs)
     def Color(self, *args, **kwargs):
-        return self._makeInstance(Color, args, kwargs)
+        return Color(*args, **kwargs)
     def Image(self, *args, **kwargs):
         return self._makeInstance(Image, args, kwargs)
     def Text(self, *args, **kwargs):
@@ -223,8 +223,7 @@ class Bot(Grammar):
 
         :return: Color object containing the color.
         '''
-        return self.Color(mode = self.color_mode, color_range = self.color_range, *args)
-        #return self.Color(*args)
+        return self.Color(mode=self.color_mode, color_range=self.color_range, *args)
 
     choice = r.choice
 
